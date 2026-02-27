7d9e2add3cd0d7e09d6da227a0a13841f16961a1
abhishekchatterjee259-personal <abhishekchatterjee259@gmail.com>
2026-02-27 16:53:10 +0530

    Reverse_Code_Added

---
+++ Reverse.py
@@ -0,0 +1,14 @@
+string="Abhsihek"
+str=""
+for i in range(len(string)-1,-1,-1):
+    print(string[i],end=" ")
+print("\n")
+
+
+string="Chatterjee"
+str=""
+for i in range(len(string)-1,-1,-1):
+    str=str+string[i]+" "
+print(str)
+
+
