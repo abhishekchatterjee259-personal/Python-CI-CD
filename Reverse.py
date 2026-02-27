3f59552bf90f9e38171a8d57d97aba962b3e4482
abhishekchatterjee259-personal <abhishekchatterjee259@gmail.com>
2026-02-27 17:17:36 +0530

    Code_Added

--- Reverse.py
+++ Reverse.py
@@ -1,14 +1,36 @@
 string="Abhsihek"
-str=""
+
 for i in range(len(string)-1,-1,-1):
     print(string[i],end=" ")
 print("\n")
 
 
 string="Chatterjee"
-str=""
+stre=""
 for i in range(len(string)-1,-1,-1):
-    str=str+string[i]+" "
+    stre=stre+string[i]+" "
 print(str)
+
+numbers = [123,121]
+for i in range(len(numbers)):
+    numbers[i] = int(str(numbers[i])[::-1])
+print(numbers)
+
+print("\n")
+list=[513,121,123,141]
+list_1=[]
+for i in list:
+    list_1.append(i)
+for j in range(len(list_1)):
+    list_1[j]=int(str(list_1[j])[-1::-1])
+print("The REVERSED LIST IS -->",list_1)
+
+for t in range(len(list)):
+    if list[t]==list_1[t]:
+        print("Palindrome-->",list[t])
+    else:
+        print("Not palindrome -->",list[t])
+
+
 
 
